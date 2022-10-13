937ed20befbe1d40c30255b3adf5985485e4d030
zadohu <zadohu22@gmail.com>
2022-10-12 20:31:45 -0500

    adds more projects

--- personal-portfolio/src/components/Card.jsx
+++ personal-portfolio/src/components/Card.jsx
@@ -1,21 +1,34 @@
+/* eslint-disable react/jsx-no-target-blank */
 import React from 'react';
 import { MdDesktopWindows } from 'react-icons/md';
 import { GoMarkGithub } from 'react-icons/go';
 
 const Card = (props) => {
 	return (
-		<div className='card w-96 h-98 bg-base-100 shadow-xl border-2 border-solid border-white p-4 place-self-center'>
+		<div className='card w-96 h-98 bg-base-100 shadow-xl border-2 border-solid border-white p-4 place-self-center mb-8'>
 			<figure>
-				<img className='h-[170px]' src={props.image} alt='project' />
+				<img
+					className='h-[250px] object-fit border-2 border-white border-solid rounded-md'
+					src={props.image}
+					alt='project'
+				/>
 			</figure>
 			<div className='card-body p-2'>
 				<h2 className='card-title'>{props.title}</h2>
 				<p className='mb-2 h-[72px]'>{props.description}</p>
 				<div className='card-actions justify-start text-3xl flex gap-6'>
-					<a className='cursor-pointer' href={props.liveLink}>
+					<a
+						className='cursor-pointer transition ease-in-out delay-75 duration-300 hover:rotate-[360deg]'
+						href={props.liveLink}
+						target='_blank'
+					>
 						<MdDesktopWindows />
 					</a>
-					<a className='cursor-pointer' href={props.repoLink}>
+					<a
+						className='cursor-pointer transition ease-in-out delay-75 duration-300 hover:rotate-[360deg]'
+						href={props.repoLink}
+						target='_blank'
+					>
 						<GoMarkGithub />
 					</a>
 				</div>
--- personal-portfolio/src/components/Projects.jsx
+++ personal-portfolio/src/components/Projects.jsx
@@ -2,19 +2,24 @@ import React from 'react';
 import Card from './Card';
 import cookBookImg from '../images/image.psd.png';
 import waterImg from '../images/water.psd.png';
+import pokemonImg from '../images/pokemon.png';
 
 // Card props are image, title, description, badgeOne, badgeTwo
 const Projects = () => {
 	return (
 		<>
-			<h1 className='text-[3em] lg:text-[5em] name text-center'>Projects</h1>
-			<div className='grid grid-cols-1 items-center place-content-center sm:grid-cols-2 md:-grid-cols-3'>
+			<h1 className='text-[3em] lg:text-[5em] name text-center mb-10'>
+				Projects
+			</h1>
+			<div className='grid grid-cols-1 items-center place-content-center md:grid-cols-2 xl:grid-cols-3'>
 				<Card
 					image={cookBookImg}
 					title={'CookBook App'}
 					description={
 						'Made with React and Firebase. Log in with Google, search, save and delete recipes to your personal Cookbook. '
 					}
+					liveLink={'https://cookbook-3b981.web.app/'}
+					repoLink={'https://github.com/zadohu22/cookbook'}
 					badgeOne={'React'}
 					badgeTwo={'Firebase'}
 				/>
@@ -25,8 +30,22 @@ const Projects = () => {
 					description={
 						'A store page where you can add items to your cart, and checkout.'
 					}
+					liveLink={'https://zadohu22.github.io/store-page-deploy/'}
+					repoLink={'https://github.com/zadohu22/store-page-deploy'}
 					badgeOne={'React'}
 					badgeTwo={'Tailwind'}
+				/>
+
+				<Card
+					image={pokemonImg}
+					title={'Pokemon Memory Game'}
+					description={`First React project, don't click on the same Pokemon twice, how high can you score?`}
+					liveLink={'https://zadohu22.github.io/memory-card-deploy/'}
+					repoLink={
+						'https://github.com/zadohu22/memory-card/tree/main/memory-card/src'
+					}
+					badgeOne={'React'}
+					badgeTwo={'API'}
 				/>
 			</div>
 		</>
